ee60b910a43010f1129d9b4d2bfd763967ab8444
Danicadel29 <danica.delfosse@student.heaj.be>
2025-10-10 12:13:09 +0200

    rythme vertical tel + btn voir plus

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,1 +1,24 @@
+'use strict';
 
+document.addEventListener('DOMContentLoaded', function() {
+    const voirPlusBtn = document.getElementById('voir-plus-btn');
+    const textContainer = document.querySelector('.text-container');
+    let isOpen = false;
+ 
+    voirPlusBtn.addEventListener('click', function(e) {
+        e.preventDefault();
+ 
+        if (!textContainer) return;
+
+        if (isOpen) {
+            // Masquer le texte
+            textContainer.style.maxHeight = '380px'; // hauteur fermée
+            voirPlusBtn.textContent = 'Voir plus';
+        } else {
+            textContainer.style.maxHeight = '600px';
+            voirPlusBtn.textContent = 'Voir moins';
+        }
+
+        isOpen = !isOpen;
+    });
+});
